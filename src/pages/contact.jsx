d13a27ce68ce6362c3d195e43fd20103099fe056
Amanda Ellis <amanda.ellis002@student.swic.edu>
2026-07-14 17:56:11 -0500

    Add Contact page component

---
+++ src/pages/contact.jsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+
+export default function Contact() {
+  return (
+    <section className="py-5">
+      <Container>
+        <h1 className="text-center mb-4">Contact Us</h1>
+        <p className="lead text-center">
+          Have questions or want to schedule an appointment? Reach out to us!
+        </p>
+      </Container>
+    </section>
+  );
+}
