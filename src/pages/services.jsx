118dd4d99b3042ce26a86f91f0367e39f28ded20
Amanda Ellis <amanda.ellis002@student.swic.edu>
2026-07-14 17:56:39 -0500

    Add Services page component

---
+++ src/pages/services.jsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+
+export default function Services() {
+  return (
+    <section className="py-5">
+      <Container>
+        <h1 className="text-center mb-4">Our Services</h1>
+        <p className="lead text-center">
+          We offer a wide range of dental services to meet your needs.
+        </p>
+      </Container>
+    </section>
+  );
+}
