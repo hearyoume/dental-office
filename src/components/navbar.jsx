0278204a34f53ce1d351dcd1d10e0f40fff3da57
Amanda Ellis <amanda.ellis002@student.swic.edu>
2026-07-14 16:34:07 -0500

    Add NavbarComponent for site navigation

---
+++ src/components/navbar.jsx
@@ -0,0 +1,26 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+export default function NavbarComponent() {
+  return (
+    <Navbar expand="lg" bg="light" data-bs-theme="light">
+      <Container fluid>
+        <Navbar.Brand href="#" className="fw-semibold">
+          Ellis Family Dentistry
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#" active>
+              Home
+            </Nav.Link>
+            <Nav.Link href="#">About</Nav.Link>
+            <Nav.Link href="#">Services</Nav.Link>
+            <Nav.Link href="#">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
